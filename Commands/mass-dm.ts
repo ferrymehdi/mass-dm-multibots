d9b14c342bf02bbce513964d290a98d2cdb18cf7
ferrymehdi <mehdiferry996@gmail.com>
2025-07-26 14:30:25 +0100

    fix(mass-dm): support Discohook URL parsing and Unicode content for mass DM

--- Commands/mass-dm.ts
+++ Commands/mass-dm.ts
@@ -1,6 +1,7 @@
 import { Colors, EmbedBuilder, Guild, SlashCommandBuilder } from "discord.js";
 import type { Command } from "../types";
 import { clients } from "..";
+import { getJsonFromURL } from "../utils/discohook";
 
 export default <Command>{
   data: new SlashCommandBuilder()
@@ -27,6 +28,31 @@ export default <Command>{
         ephemeral: true,
       });
     }
+
+    const DISCOHOOK_URL_REGEX =
+      /^(https?:\/\/)?(www\.)?(discohook\.app|discohook\.org)\/\?data=/;
+    let content = message.trim();
+
+    const embeds: any[] = [];
+    if (DISCOHOOK_URL_REGEX.test(message)) {
+      const discohook = getJsonFromURL(message);
+      if (discohook) {
+        if (discohook.embeds && discohook.embeds.length > 0) {
+          for (const embed of discohook.embeds) {
+            embeds.push(embed);
+          }
+        }
+        if (discohook.content) {
+          content = discohook.content;
+        }
+      }
+    }
+
+    const variables: Map<string, string> = new Map();
+
+    variables.set("guild.name", guild.name);
+    variables.set("guild.id", guild.id);
+
     const totalClients = clientsInGuild.length;
     const allMembers = guild.members.cache.filter((m) => !m.user.bot);
     const done: { clientId: string; done: number; failed: number }[] =
@@ -67,13 +93,28 @@ export default <Command>{
       if (!clientsInGuild[clientIndex]) {
         clientIndex = 0;
       }
+      variables.set("user.username", member.user.username);
+      variables.set("user.id", member.user.id);
+      variables.set("user.tag", member.user.tag);
+      variables.set("user.avatar", member.user.displayAvatarURL());
+      variables.set("user.mention", member.user.toString());
+
       const client = clientsInGuild[clientIndex] as any;
       const clientDone = done.find(
         (d) => d.clientId === client.user?.id,
       ) as any;
       clientIndex = (clientIndex + 1) % totalClients;
       try {
-        await client.users.send(member.user.id, message);
+        // await client.users.send(member.user.id, message);
+        for (const [key, value] of variables.entries()) {
+          // Replace {key} with value in content
+          content = content.replace(new RegExp(`{${key}}`, "g"), value);
+        }
+
+        await client.users.send(member.user.id, {
+          content: content,
+          embeds: embeds,
+        });
         clientDone.done++;
       } catch (error) {
         console.error(`Failed to send message to ${member.user.tag}:`, error);
